95def4143f67640f2a222cb73a5d57936c9bbad9
paulxd215 <jymyatao12345678@gmail.com>
2025-05-11 16:02:18 -0500

    Ya

---
+++ bodega-ayacucho/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/Header.css";
+const Header = () =>{
+
+    return(
+      <div>
+<p>
+    marcel manco
+</p>
+      </div>
+
+  );
+};
+
+
+export default Header; 
