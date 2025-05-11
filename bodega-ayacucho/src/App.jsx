6f66bd8a6c051ee31f093f8536675c7e49e3417e
Kenneth Molina <kenneth-molina899@outlook.es>
2025-05-11 08:28:29 -0500

    primerComponente

--- bodega-ayacucho/src/App.jsx
+++ bodega-ayacucho/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import "./styles/Footer.css";
+import Footer from './components/Footer';
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Footer/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error saepe quo itaque praesentium pariatur? Ea doloremque perspiciatis earum, reiciendis nisi voluptate eveniet iusto nobis ipsum fuga, sequi dolor harum mollitia.</p>
       
     </>
   )
---
+++ bodega-ayacucho/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/Footer.css";
+const Footer = () =>{
+    return(
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, harum dolorum dolores nihil a impedit obcaecati dicta beatae facilis consequuntur laudantium mollitia. Fugit minima, sapiente ab eum ipsum esse veritatis!</p>
+        </div>
+    )
+}
+
+export default Footer;
